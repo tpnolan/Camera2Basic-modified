74ac3c18f1382bb7d7b31ae60d5bcc87d133eb4c
Hani Elabed <hani.elabed@gmail.com>
2019-01-10 08:53:46 -0600

    added proximity sensor and gyroscope
    and got the proximity sensor to display its info
    upon opening the DisplaySensorDataActivity
    window. WIP.

--- Application/src/main/java/com/example/android/camera2basic/DisplaySensorDataActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/DisplaySensorDataActivity.java
@@ -1,11 +1,19 @@
 package com.example.android.camera2basic;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.TextView;
 
 public class DisplaySensorDataActivity extends AppCompatActivity {
+
+    /**
+     * Tag for the {@link Log}.
+     */
+    private static final String TAG = "DisplaySensorDataActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +26,22 @@ public class DisplaySensorDataActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        String message = "Sensor data here...";
+        SensorManager sensorManager =
+                (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        Sensor proximitySensor =
+                sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        String message = "Sensor data here...\n";
+
+        if(proximitySensor == null) {
+            message = "Proximity sensor not available.";
+            Log.e(TAG, message);
+        } else {
+            message += "Proximity sensor is available.\n";
+            message += proximitySensor.toString();
+            Log.e(TAG, message);
+        }
 
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
