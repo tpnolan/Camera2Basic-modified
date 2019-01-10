792b1960a57a29fb43e44c450840a5d094c8ea09
Hani Elabed <hani.elabed@gmail.com>
2019-01-09 22:02:16 -0600

    now the actionBar or Toolbar with
    the back button are working.
    Hurray

--- Application/src/main/java/com/example/android/camera2basic/DisplaySensorDataActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/DisplaySensorDataActivity.java
@@ -2,6 +2,7 @@ package com.example.android.camera2basic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class DisplaySensorDataActivity extends AppCompatActivity {
@@ -10,6 +11,12 @@ public class DisplaySensorDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_sensor_data);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         String message = "Sensor data here...";
 
